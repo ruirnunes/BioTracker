573fe1868b69a4b5d6af71f2dbae72b333ec5f3c
ruirnunes <rui.r.b.nunes@gmail.com>
2026-03-28 11:35:48 +0000

    feat: update environment configuration for production

--- frontend/src/app/core/services/api.ts
+++ frontend/src/app/core/services/api.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
+
 @Injectable({ providedIn: 'root' })
 export class ApiService {
   private http = inject(HttpClient);
